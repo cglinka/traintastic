ccbfa44747c6dfccdc0c65f6e442774a0f6ab36c
Nik Daftary <nikdaftary@gmail.com>
2013-09-20 10:22:13 -0500

    the foundations of a beautiful building

--- building.rb
+++ building.rb
@@ -1,2 +1,12 @@
 class Building
+	attr_accessor :windows, :doors, :floors, :gargoyles
+	def initialize (windows, doors, floors)
+		@windows = windows
+		@doors = doors
+		@floors = []
+		@gargoyles = gargoyles
 end
+
+
+
+#hello world. 
